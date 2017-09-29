c9d35a885b0c004255c7513337592c858b9e8e8a
Erwin Gonzalez <erwinggonzalez@galileo.edu>
2017-09-29 11:13:12 -0600

    Using Fragments Example

--- MicroMaster_FragmentsExample/app/src/main/java/com/galileo/micromaster/fragmentsexample/MainActivity.java
+++ MicroMaster_FragmentsExample/app/src/main/java/com/galileo/micromaster/fragmentsexample/MainActivity.java
@@ -11,6 +11,7 @@ import static android.os.Build.VERSION_CODES.M;
 
 public class MainActivity extends AppCompatActivity {
 
+    //variable used to change the behavior of the button
     public boolean show_frag1 = false;
 
     @Override
@@ -19,13 +20,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FirstFragment fragment = new FirstFragment();
+        //fragment transaction, adds the first fragment to the activity
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.fragment_container, fragment).commit();
     }
 
+    //button onclick method
     public void changeFragment(View view) {
+        //declares the fragment that will be shown
         Fragment fragment;
+        //finds the button to change the label
         Button button = (Button)findViewById(view.getId());
+        //decides if we have to show the first or the second fragment
+        //then creates the fragment and changes the button label
         if(!show_frag1){
             fragment = new SecondFragment();
             button.setText(R.string.show_first_fragment);
@@ -33,8 +40,10 @@ public class MainActivity extends AppCompatActivity {
             fragment = new FirstFragment();
             button.setText(R.string.show_second_fragment);
         }
+        //replaces the fragment being shown
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, fragment).commit();
+        //then changes the condition variable
         show_frag1 = !show_frag1;
     }
 }
